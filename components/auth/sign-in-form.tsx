9d21bacfca5d8877e0cb1a2de6bcb4ecffdb96c1
humbl3LilaS <sabishinekobebe@gmail.com>
2025-01-20 14:45:14 +0630

    add: navigation link and header in sign-in and sign-up form

--- components/auth/sign-in-form.tsx
+++ components/auth/sign-in-form.tsx
@@ -5,6 +5,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const SignInForm = () => {
     const form = useForm<TSignInSchema>({
@@ -18,6 +19,10 @@ const SignInForm = () => {
         <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)}>
                 <div className={"flex flex-col gap-y-4"}>
+                    <h1 className={"text-2xl font-semibold font-white"}>
+                        Welcome back to BookWise
+                    </h1>
+                    <p>Access the vast collection of resource, and stay updated</p>
                     <FormField
                         control={form.control}
                         name={"email"}
@@ -56,6 +61,12 @@ const SignInForm = () => {
                     </Button>
                 </div>
             </form>
+            <p className={"mt-3 text-center font-medium"}>
+                New to BookWise?&nbsp;
+                <Link href={"/sign-in"} className={"font-bold text-primary"}>
+                    Create new account
+                </Link>
+            </p>
         </Form>
     );
 };
--- components/auth/sign-up-form.tsx
+++ components/auth/sign-up-form.tsx
@@ -5,6 +5,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const SignUpForm = () => {
     const form = useForm<TSignUpSchema>({
@@ -17,6 +18,13 @@ const SignUpForm = () => {
         <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)}>
                 <div className={"flex flex-col gap-y-4"}>
+                    <h1 className={"text-2xl font-semibold font-white"}>
+                        Welcome back to BookWise
+                    </h1>
+                    <p>
+                        Please complete all fields and upload a valid id to gain access to the
+                        library
+                    </p>
                     <FormField
                         control={form.control}
                         name={"fullName"}
@@ -90,6 +98,12 @@ const SignUpForm = () => {
                     </Button>
                 </div>
             </form>
+            <p className={"mt-3 text-center font-medium"}>
+                Already have an account?{" "}
+                <Link href={"/sign-in"} className={"font-bold text-primary"}>
+                    Sign In
+                </Link>
+            </p>
         </Form>
     );
 };
