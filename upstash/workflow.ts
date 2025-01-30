197126dfbc3f25ec74137b23979bef1dbe2942b6
humbl3LilaS <sabishinekobebe@gmail.com>
2025-01-30 20:46:54 +0630

    fix: sendEmail from error v2

--- upstash/workflow.ts
+++ upstash/workflow.ts
@@ -25,7 +25,7 @@ export const sendEmail = async ({ email, subject, message }: SendEmailPayload) =
             }),
         },
         body: {
-            from: "Pale Edelweiss <contact@pale-edelweiss.tech>",
+            from: "PaleEdelweiss <noti@pale-edelweiss.tech>",
             to: [email],
             subject,
             html: message,
