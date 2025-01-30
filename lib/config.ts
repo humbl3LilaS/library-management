730b99db6947c56fa9e8e3e961aced7fa7ecd342
humbl3LilaS <sabishinekobebe@gmail.com>
2025-01-30 21:10:55 +0630

    fix: config again :'}

--- lib/config.ts
+++ lib/config.ts
@@ -7,8 +7,8 @@ const config = {
             endPoint: process.env.NEXT_PUBLIC_IMAGE_KIT_URL_ENDPOINT!,
         },
         upstash: {
-            qstashToken: process.env.UPSTASH_QSTASH_TOKEN!,
-            qstashUrl: process.env.UPSTASH_QSTASH_URL!,
+            qstashToken: process.env.QSTASH_TOKEN!,
+            qstashUrl: process.env.QSTASH_URL!,
         },
         resend: {
             token: process.env.RESEND_API_KEY!,
